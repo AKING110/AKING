4c5639c95ce3e8a52f8c32e01da6e18bedaad3db
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-08-02 06:19:53 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -6,11 +6,13 @@ def Run():
         if bit == '64bit':                                                                          
             print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
             print('[•] This Tools is Paid Not Free')
+            os.system('xdg-open https://www.facebook.com/groups/351076900316263/permalink/374959374594682/')
             import AKING
             AKING.Main()
         elif bit == '32bit':
             print("\n\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
             print('[•] This Tools is Paid Not Free')
+            os.system('xdg-open https://www.facebook.com/groups/351076900316263/permalink/374959374594682/')
             import AKING32
             AKING32.Main()
         else:
