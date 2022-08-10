4c4f0cfa234185e15f8d7ade1b497f5dee0a2c03
sallukhan554 <110468131+sallukhan554@users.noreply.github.com>
2022-08-10 20:07:58 +0500

    Update and rename AKING.py to ahmad.py

--- ahmad.py
+++ ahmad.py
@@ -10,10 +10,10 @@ logo = ("""\033[1;37m    ###    ##    ## #### ##    ##  ######  ❤
  ##     ## ##   ##   ##  ##   ### ##    ##
  ##     ## ##    ## #### ##    ##  ######  ❤
 (!)══════════════════════════════════════════
-(!) Author   : IMTIAZ AKING
-(!) Guthub   : AKING110
+(!) Author   : ahmad khan
+(!) Guthub   : ahmad
 (!) Facebook : MR.AKING.07
-(!) Type     : PAID
+(!) Type     : free
 (!) Version  : 1.2.1
 \033[1;37m(!)══════════════════════════════════════════""")
 
@@ -24,11 +24,11 @@ def Run():
 	print('[•] Choose Your Country For Cloning\n\033[1;37m(!)══════════════════════════════════════════')
 	print('[1] Pak Cloning \n[2] BD Cloning\n[0] Exit')
 	Aking = input('[•] Choose : ')
-	if Aking =='1':
+	if free =='1':
 		if bit =='32bit':
-			import AKING32
+			import free32
 		elif bit =='64bit':
-			import AKING
+			import free
 	elif Aking =='2':
 		if bit =='32bit':
 			import BD32
