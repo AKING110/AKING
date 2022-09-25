416e6a05103ade2c14e0c9e60dcc2682ba3359d0
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-09-25 18:28:38 +0500

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,10 @@
+from platform import uname
+arch=uname().machine.lower()
+if "aarch" in arch:
+    arch="aarch"
+    print('\n\033[1;32mCongratulations! Your Device Support This Tools\033[1;37m')
+else:
+    exit("\033[1;31mSystem Not Support This Tools\033[1;37m")
+while True:
+import AKING
+exit('\n\n Something Working was wrong \n connect on wp -> +923237528063')
