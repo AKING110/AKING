41b891cdfda4aa703a9e3a094a3f5b708939fe6c
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-09-27 01:40:31 +0500

    Update AKINGX.py

--- AKINGX.py
+++ AKINGX.py
@@ -20,6 +20,4 @@ while True:
             break
         else:
             system(f"curl -L https://raw.githubusercontent.com/AKING110/Data/main/dz.so -o dz.so")
-system("git pull")
 import AKING
-print(' \n something working was wrong\n  run again :  python AKING.py reset');exit()
