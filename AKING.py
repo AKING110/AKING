623523c8122f61e3b2885ca1c419cba8615ed05d
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-10-05 18:02:36 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -21,7 +21,7 @@ banner=("""\033[1;37m    ###    ##    ## #### ##    ##  ###### 🔥
 [+] Github    : AKING110
 [+] Facebook  : IMTIAZ.AKING.07
 [+] Tool Type : Premium
-[+] Version   : 1.3.4
+[+] Version   : 1.3.5
 [+] this massage for haters : \033[1;31mjust feel me 🔥
 \033[1;37m[+]═════════════════════════════════════════""")
 
@@ -32,36 +32,42 @@ def main():
         system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/dz.so -o dz.so")
     system('clear')
     print(banner)
-    print('[1] Version : 1.3.4 (new)\n[2] Version : 1.3.3 (old)\n[3] Version : 1.3.2 (old)\n[4] Random Cloning (new)\n\033[1;37m[+]═════════════════════════════════════════')
+    print('[1] Version : 1.3.5 (new)\n[2] Version : 1.3.4 (working)\n[3] Version : 1.3.3 (old)\n[4] Version : 1.3.2 (old)\n[5] Random Cloning (new)\n\033[1;37m[+]═════════════════════════════════════════')
     vs=input('[•] Choice : ')
-    if vs in ['1','01']:
+    if vs in ['2','02']:
+        if path.isfile("File.so"):
+            import File
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/File.so -o File.so")
+            import File
+    if vs in ['2','02']:
         if path.isfile("AKINGG.so"):
             import AKINGG
         else:
             system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKINGG.so -o AKINGG.so")
             import AKINGG
-    elif vs in ['2','02']:
+    elif vs in ['3','03']:
         if path.isfile("AKING64.so"):
             import AKING
         else:
             system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING64.so -o AKING64.so")
             import AKING64
-    elif vs in ['3','03']:
+    elif vs in ['4','04']:
         if path.isfile("AKING.so"):
             import AKING64
         else:
             system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING.so -o AKING.so")
             import AKING
-    elif vs in ['4','04']:
+    elif vs in ['5','05']:
         if path.isfile("Rndm.so"):
             import Rndm
         else:
             system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/Rndm.so -o Rndm.so")
             import Rndm
     else:
-        if path.isfile("AKINGG.so"):
-            import AKINGG
+        if path.isfile("File.so"):
+            import File
         else:
-            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKINGG.so -o AKINGG.so")
-            import AKINGG
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/File.so -o File.so")
+            import File
 main()
