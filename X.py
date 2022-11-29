4022bb06e2853ba215737e0beb804b1b8f8f7322
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-11-29 15:13:44 +0500

    Update X.py

--- X.py
+++ X.py
@@ -5,36 +5,22 @@ from os import path,system
 arch=uname().machine.lower()
 if "aarch" in arch:
     arch="aarch"
-    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools');time.sleep(1)
+    print('\033[1;32m\n Congratulatings! Your Deviec Support This Tools');time.sleep(1)
 else:
-    print('\033[1;31m\nSorry System not support this tools');sys.exit()
-
+    print('\033[1;31m\n Sorry System not support this tools');sys.exit()
 try:
     if sys.argv[1]=='update':
         system('cd $HOME && cd AKING && rm -f *')
         system("curl -L https://raw.githubusercontent.com/AKING110/AKING/main/AKING.py -o AKING.py && python AKING.py")
 except:
     pass
-if path.isfile("dz.so"):
+if path.isfile("dump.so"):
     pass
 else:
-    system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/dz.so -o dz.so")
-system('clear')
-print('\n\n\033[1;37m[•] This tools only for 64bit device ')
-print('\n[1] Start Cloning V1.3.9 \n[2] Start Random Cloning V1.3.8 (Updated)\n[3] Check Update \n')
-xd=input('[•] choose: ')
-if xd in ['1','01']:
-    if path.isfile('AKING.so'):
-        import AKING
-    else:
-        system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING.so -o AKING.so")
-        import AKING
-elif xd in ['2','02']:
-    if path.isfile('Random.so'):
-        import Random
-    else:
-        system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/Random.so -o Random.so")
-        import Random
+    system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/dump.so -o dump.so")
+if path.isfile("AKING.so"):
+    pass
 else:
-        print('\n[•] Checking updates...')
-        system('python AKING.py update')
+    system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING.so -o AKING.so")
+    import AKING
+    AKING.menu()
