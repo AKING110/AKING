2958124acae1341030d71b567acbc83b2afab097
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-06-19 17:27:12 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,6 +1,7 @@
-import os 
+import os
 os.system('xdg-open https://chat.whatsapp.com/FwFmZyWx4X3GQyxV4jEtSU')
-os.system('git pull')                                      from os import path,system                                 from platform import uname
+os.system('git pull')
+from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
   os.system('chmod 777 New;./New')
